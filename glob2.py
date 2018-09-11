598d8a54be895fce063b718713dd206a2fe8fe93
thispassing <thispassing@users.noreply.github.com>
2018-09-10 20:48:10 -0400

    done adding old things for now

--- glob2.py
+++ glob2.py
@@ -1,7 +1,10 @@
 import glob2
 from datetime import datetime
 
-filenames = glob2.glob(".txt")
+# originally this was filesnames = glob2.glob('*.txt'), but i don't think it worked with all the other .txt
+# files in this folder, so i just changed it for now, can test in another location one time
+
+filenames = ['file1.txt', 'file2.txt', 'file3.txt']
 with open(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f"+".txt"), "w") as file:
     for filename in filenames:
         with open(filename, "r") as f:
