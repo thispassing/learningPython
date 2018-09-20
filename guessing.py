f8c20efb3e870eae683315c27d206fe9dcc38665
thispassing <thispassing@users.noreply.github.com>
2018-09-20 10:05:44 -0400

    fixing

--- guessing.py
+++ guessing.py
@@ -10,17 +10,16 @@ while guess != number:
         guess = int(input("Too high, try again: "))
     if guess == number:
         print("Winner!")
-        while True:
-            play_again = input("Do you want to keep playing? (y/n) ")
-            play_again = play_again.lower()
-            if play_again == "y":
-                number = randint(1,10)
-                guess = int(input("Guess a number between 1 and 10: "))
-            elif play_again == "n":
-                print("OK, cya next time!")
-            elif play_again == "stop":
-                print("OK, I'll stop.")
-                break
-            else:
-                print("Sry, I don't understand that.")
+        play_again = input("Do you want to keep playing? (y/n) ")
+        play_again = play_again.lower()
+        if play_again == "y":
+            number = randint(1,10)
+            guess = int(input("Guess a number between 1 and 10: "))
+        elif play_again == "n":
+            print("OK, cya next time!")
+        elif play_again == "stop":
+            print("OK, I'll stop.")
+            break
+        else:
+            print("Sry, I don't understand that.")
 
