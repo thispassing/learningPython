668518215b3ad679d1d9007c08182154cc6db513
thispassing <thispassing@users.noreply.github.com>
2018-09-20 12:19:59 -0400

    guessing game v2

--- guessing2.py
+++ guessing2.py
@@ -1,26 +1,27 @@
 from random import randint
 
 
-guess = int(input("Guess a number between 1 and 10: "))
+guess = None
 number = randint(1,10)
 
-while guess != number:
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
     if guess < number:
-        guess = int(input("Too low, try again: "))
+        print("Too low, try again.")
     elif guess > number:
-        guess = int(input("Too high, try again: "))
-    if guess == number:
+        print("Too high, try again.")
+    else:
         print("Winner!")
         play_again = input("Do you want to keep playing? (y/n) ")
         play_again = play_again.lower()
         if play_again == "y":
-            guess = int(input("Guess a number between 1 and 10: "))
             number = randint(1,10)
         elif play_again == "n":
             print("OK, cya next time!")
+            break
         elif play_again == "stop":
             print("OK, I'll stop.")
             break
         else:
             print("Sry, I don't understand that.")
-
+            continue
