b4a2ee5c8731b1a9e5ab7e23a7775d3231f69200
thispassing <thispassing@users.noreply.github.com>
2018-09-20 07:34:29 -0400

    more loop work

---
+++ more_loops3.py
@@ -0,0 +1,42 @@
+# user_response = None
+# while user_response != "please":
+#     user_response = input("You didn't say the magic word: ")
+# print("ty")
+
+# msg = input("what's the secret password? ")
+# while msg != "bananas":
+#     print("WRONG!")
+#     msg = input("what's the secret password? ")
+# print("CORRECT!")
+
+#for num in range(1,11):
+    #print(num)
+
+# num = 1
+# while num < 11:
+#     print(num)
+#     num += 1
+
+import time
+
+# smile = "\U0001f600"
+# while smile:
+#     print(smile)
+#     smile += " \U0001f600"
+#     time.sleep(2)
+
+# smile = "\U0001f600"
+# for x in range(1,10):
+#     while smile:
+#         print(smile)
+#         smile += " \U0001f600"
+
+for x in range(3):
+    for num in range(1,11):
+        print("\U0001f600 " * num)
+
+# times = 1
+# while times <= 10:
+#     print ("\U0001f600 " * times)
+#     times += 1
+
